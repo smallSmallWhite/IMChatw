d94a8ebc1ea0c2ba604a106f8e369072ee7cbc87
nbns <18291820539@163.com>
2017-01-25 19:00:18 -0800

    no message

--- IMChat/MPConfigDelegate.m
+++ IMChat/MPConfigDelegate.m
@@ -16,6 +16,7 @@
     NIMNotificationContent *content = [notification content];
     if ([content isKindOfClass:[NIMTeamNotificationContent class]]) //这里做个示范如何忽略部分通知 (不在聊天界面显示)
     {
+        
         NSArray *types = [[NTESBundleSetting sharedConfig] ignoreTeamNotificationTypes];
         NIMTeamOperationType type = [(NIMTeamNotificationContent *)content operationType];
         for (NSString *item in types)
