8650ca60138225bad000467da6951e3ee7b57202
nbns <18291820539@163.com>
2017-01-25 16:36:13 -0800

    创建一个即时通讯项目

---
+++ IMChat/IMChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IMChat
+//
+//  Created by admin on 2017/1/25.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
